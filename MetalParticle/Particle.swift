3bf1a3b037af626ffda7fe108a6302458b7eee94
Domin Kim <doremin98@gmail.com>
2025-07-02 23:33:32 +0900

    [ADD] Particle 자료형

---
+++ MetalParticle/Particle.swift
@@ -0,0 +1,19 @@
+//
+//  Particle.swift
+//  MetalParticle
+//
+//  Created by doremin on 7/2/25.
+//
+
+import simd
+
+// MARK: - Particle Structure
+struct Particle {
+    var position: simd_float2
+    var velocity: simd_float2
+    var life: Float
+    var maxLife: Float
+    var scale: Float
+    var textureCoord: simd_float2
+    var tileSize: Float
+}
